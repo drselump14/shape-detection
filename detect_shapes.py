77f52e86c628c9c526302bda54249a29c8a244f7
drselump14 <botoksgonzales@gmail.com>
2017-04-14 10:55:51 +0900

    enable detection for white background

--- detect_shapes.py
+++ detect_shapes.py
@@ -6,6 +6,8 @@ from pyimagesearch.shapedetector import ShapeDetector
 import argparse
 import imutils
 import cv2
+import pdb
+import numpy as np
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -17,14 +19,37 @@ args = vars(ap.parse_args())
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
 resized = imutils.resize(image, width=300)
+# pdb.set_trace()
 ratio = image.shape[0] / float(resized.shape[0])
+
+lower_white = np.array([220, 220, 220], dtype=np.uint8)
+upper_white = np.array([255, 255, 255], dtype=np.uint8)
+mask = cv2.inRange(resized, lower_white, upper_white)  # cou
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+mask = cv2.bitwise_not(mask)
+# cv2.imshow("Image", mask)
+# cv2.waitKey(0)
+
+# load background (could be an image too)
+bk = np.full(resized.shape, 255, dtype=np.uint8)  # white bk
+# get masked foreground
+fg_masked = cv2.bitwise_and(resized, resized, mask=mask)
+cv2.imshow("Image", fg_masked)
+cv2.waitKey(0)
+
+# get masked background, mask must be inverted
+# mask = cv2.bitwise_not(mask)
+# bk_masked = cv2.bitwise_and(bk, bk, mask=mask)
+# cv2.imshow("Image", bk_masked)
+# cv2.waitKey(0)
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(fg_masked, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-
+cv2.imshow("Image", thresh)
+cv2.waitKey(0)
 # find contours in the thresholded image and initialize the
 # shape detector
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
@@ -48,7 +73,7 @@ for c in cnts:
 	c = c.astype("int")
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (255, 255, 255), 2)
+		0.5, (0,0,0), 2)
 
 	# show the output image
 	cv2.imshow("Image", image)
